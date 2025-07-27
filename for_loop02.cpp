ba92c03adb44bc2f7df17974928a831708965d02
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-27 08:52:01 +0530

    for_loop

---
+++ for_loop02.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int siddharth;
+    for(siddharth =1;siddharth<=20;siddharth++)
+    cout<<"  \nsiddharth";
+    cout<<endl;
+    return 0;
+}
---
+++ for_loop06.cpp
@@ -0,0 +1,10 @@
+//print the number from 1 to 100
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    cout<<("\n")<<i;
+    return 0;
+}
