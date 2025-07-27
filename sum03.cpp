90b04ea5c9c7d043fc1c98c7f186db1a8936ac2a
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-27 08:52:53 +0530

    Sum

---
+++ sum03.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B,Add;
+    cout<<"enter the first number given by the user:\n";
+    cin>>A;
+    cout<<"Enter the second number given by the user is:\n";
+    cin>>B;
+    Add=A+B;
+    cout<<"sum of the two number given by the user is:\n"<<Add;
+    return 0;
+
+}
---
+++ sum05.cpp
@@ -0,0 +1,12 @@
+//sum of all number in one line
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,add;
+    cout<<"enter the three number:";
+    cin>>a>>b>>c;
+    add=a+b+c;  
+    cout<<"sum of all number is:"<<add;
+    return 0;
+}
