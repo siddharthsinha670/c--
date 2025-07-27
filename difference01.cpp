a2c239be33e30f9ab02f4e49354755436a557f61
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-27 08:56:46 +0530

    Difference

---
+++ difference01.cpp
@@ -0,0 +1,11 @@
+//differnce of two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sub;
+    cout<<"enter the two number:\n";
+    cin>>a>>b;
+    sub=a-b;
+    cout<<"differnce of two number:\n"<<sub;
+    return 0;
+}
