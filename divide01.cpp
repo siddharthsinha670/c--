de3d44f3bd0e2fb421f957221e8d20571eb96019
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-27 08:56:58 +0530

    Divide

---
+++ divide01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,divide;
+    cout<<"enter the first number:\n";
+    cin>>a;
+    cout<<"enter the second number:\n";
+    cin>>b;
+    divide=b/a;
+    cout<<"divide the given number:\n"<<divide;
+    return 0;
+
+}
