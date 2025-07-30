92afddcca525f749e41d69358a7cf4cb092f4165
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-30 23:10:19 +0530

    if else statement

---
+++ if_else02.cpp
@@ -0,0 +1,18 @@
+//program of if_else statement
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<("Enter two number\n");
+    cin>>a>>b;//a=2,b=4
+    if(a>b)
+    {
+        cout<<"a="<<a;
+    }
+    else
+    {
+        cout<<"b="<<b;
+    }
+    return 0;
+}
