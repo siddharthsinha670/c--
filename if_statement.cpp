ea1ef28c74cda76fba28e1aab17b9046ccef132e
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-07-30 23:10:42 +0530

    if statement

---
+++ if_statement.cpp
@@ -0,0 +1,15 @@
+//write a program to show the simple example of if statement
+#include<iostream>
+using namespace std;
+int main()
+{
+    int atm;
+    cout<<"Enter atm pin\n";
+    cin>>atm; //we have entered our atm pin here in output area it will responsible for that.
+    if(atm=1234)
+    {
+        cout<<("you can access your balance.");
+        
+    }
+    return 0;
+}
